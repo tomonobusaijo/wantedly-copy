3fed7fdbf5336ee90d79480bf14253fac66f2f30
Tomonobu Saijo <tomonobu.saijo@gmail.com>
2016-11-11 13:09:39 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root 'projects#index'
+  # unauthenticated do   # ログインしていない場合のルーティング(コントローラー指定)／asでPrefixを定義
+  #   root "devise/sessions#new", as: 'unauthenticated_root'
+  # end
+
+  resources :users
+  resources :projects
 end
